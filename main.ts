2b8f4cd7d8c60a18d51e1f07d469c0c59dce0f3e
berkaycimsir <berkaycimsir2003@hotmail.com>
2022-08-13 17:28:25 +0300

    initial commit

---
+++ main.ts
@@ -0,0 +1,15 @@
+import blog from 'https://deno.land/x/blog@0.4.2/blog.tsx';
+
+blog({
+  title: 'Berkay Cimsir',
+  avatar: 'https://avatars.githubusercontent.com/u/47090177?v=4',
+  avatarClass: 'rounded-full border-none',
+  author: 'Berkay Cimsir',
+  description: 'Jr. software engineer, student.',
+  dateStyle: 'long',
+  theme: 'light',
+  links: [
+    { title: 'Email', url: 'mailto:berkaycimsir2003@hotmail.com' },
+    { title: 'GitHub', url: 'https://github.com/berkaycimsir' },
+  ],
+});
---
+++ posts/hello_world.md
@@ -0,0 +1,6 @@
+---
+title: Hello world!
+publish_date: 2022-08-13
+---
+
+This is my first blog post!
